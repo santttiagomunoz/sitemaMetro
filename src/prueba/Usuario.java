5c7216313faefeceeb5ec96af921888f0a7c8d65
santiago <124795721+santttimunoz@users.noreply.github.com>
2024-09-25 21:37:57 -0500

    ajuste en el flujo del codigo

--- src/prueba/Usuario.java
+++ src/prueba/Usuario.java
@@ -10,97 +10,108 @@ public class Usuario {
     static String id = null, nombre, correo, contra, idConfirm, contraConfirm;
     static boolean frecuente, casual;
     static double balance = 0, recarga;
-    static int  typeTrip;
+    static int typeTrip;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Menu();
     }
 
-    public static void Registrar(){
+    public static void Registrar() {
         System.out.println("ingrese su id: ");
         id = sc.next();
         System.out.println("ingrese su nombre:");
-        nombre = sc.nextLine();
+        nombre = sc.next();
         System.out.println("ingrese su correo:");
-        correo = sc.nextLine();
+        correo = sc.next();
         System.out.println("ingrese contraseña:");
-    }
-    public static void iniciarSesion(){
+        contra = sc.next();
 
-            System.out.println("ingrese su id:");
-            idConfirm = sc.next();
-            System.out.println("ingrese contraseña:");
-            contra = sc.next();
-            if(idConfirm.equals(correo) && contraConfirm.equals(contra)){
-                frecuente = true;
-                System.out.println("Bienvenido "+ nombre);
+    }
+
+    public static void iniciarSesion() {
+
+        System.out.println("ingrese su id:");
+        idConfirm = sc.next();
+        System.out.println("ingrese contraseña:");
+        contraConfirm = sc.next();
+        if (idConfirm.equals(id) && contraConfirm.equals(contra)) {
+            frecuente = true;
+            String answer;
+            do {
+                System.out.println("Bienvenido " + nombre);
                 System.out.println("tu saldo es: " + balance);
                 System.out.println("1- Recargar " + "\n" + "2- viajar");
                 int op = sc.nextInt();
-                String answer;
-                do{
-                    switch (op){
-                        case 1: Recargar();
-                            break;
-                        case 2: Viajar();
-                            break;
-                        default:
-                            System.out.println("ingrese una opcion valida");
-                    }
-                    System.out.println("Quieres recargar o viajar de nuevo? s/n");
-                    answer = sc.next();
-                }while(answer.equals("s"));
+                switch (op) {
+                    case 1:
+                        Recargar();
+                        break;
+                    case 2:
+                        Viajar();
+                        break;
+                    default:
+                        System.out.println("ingrese una opcion valida");
+                }
+                System.out.println("Quieres recargar o viajar? s/n");
+                answer = sc.next();
+            } while (answer.equals("s"));
 
-            }else{
-                casual = true;
-                System.out.println("no estas registrado");
-            }
+        } else {
+            casual = true;
+            System.out.println("no estas registrado");
             System.out.println("!REGISTRATE¡");
+        }
     }
-    public static void Recargar(){
+
+    public static void Recargar() {
         System.out.println("Ingrese el valor a recargar: ");
         recarga = sc.nextDouble();
         balance = balance + recarga;
     }
-    public static void Viajar(){
-        if(balance > 3500){
+
+    public static void Viajar() {
+        if (balance > 3500) {
             System.out.println("tipo de viaje---");
-            System.out.println("1- tranvia" + "\n" + "2- bus" + "\n" +"3- metrocable" + "\n" +"4- integrado");
+            System.out.println("1- tranvia" + "\n" + "2- bus" + "\n" + "3- metrocable" + "\n" + "4- integrado");
             typeTrip = sc.nextInt();
-            switch (typeTrip){
+            switch (typeTrip) {
                 case 1:
-                    System.out.println("no pagas valor adicional");
-                    break;
-                case 2: balance = balance - 3800;
-                    System.out.println("pagas 300$ de valor adicional");
                 case 3:
-                    System.out.println("no pagas valor adicional");
+                    System.out.println("no pagas valor adicional"+ "\n" + "valor del pasaje: 3600$" + "\n" + "saldo: " + balance);
+                    break;
+                case 2:
+                    balance = balance - 3800;
+                    System.out.println("valor adicional: 200$" + "\n" + "valor del pasaje: 3600$" + "\n" + "saldo: " + balance);
                     break;
                 case 4:
                     balance = balance - 4100;
+                    System.out.println("valor adicional: 500$" + "\n" + "valor del pasaje: 3600$" + "\n" + "saldo: " + balance);
+                    break;
                 default:
                     System.out.println("seleccione una opcion valida");
             }
-        }else if(balance < 3500 && balance >= 0){
+        } else if (balance < 3500 && balance >= 0) {
             balance = balance - 3500;
+            System.out.println("saldo: " + balance);
             System.out.println("te prestamos un pasaje");
-        }else{
+        } else {
             System.out.println("no tienes saldo y debes un pasaje, debes recargar");
         }
 
     }
-    public static void Menu(){
+
+    public static void Menu() {
         String volver;
-        do{
+        do {
             System.out.println("Sistema metreo----");
-            System.out.println("1- Resgistrar"+ "\n" + "2- Iniciar sesion");
+            System.out.println("1- Resgistrar" + "\n" + "2- Iniciar sesion");
             System.out.println("ingrese una opcion: ");
             int op = sc.nextInt();
-            switch (op){
-                case 1 :
+            switch (op) {
+                case 1:
                     Registrar();
                     break;
-                case 2 :
+                case 2:
                     iniciarSesion();
                     break;
                 default:
@@ -108,7 +119,7 @@ public class Usuario {
             }
             System.out.println("desea volver al sistema? s/n");
             volver = sc.next();
-        }while(volver.equals("s"));
+        } while (volver.equals("s"));
 
     }
 }
